ce5ec81008ec7635ab6fd57ed1ba3bed8b0782fe
Reazul Islam <reazulislam137@gmail.com>
2018-05-03 14:11:01 +0600

    first commit

---
+++ may-03-01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << 1+2 << endl;
+    cout << 4/2 << endl;
+    cout << 34%5 << endl;
+    cout << 3 * 6 << endl;
+
+    double myVariable;
+
+    myVariable = 50;
+
+    cout << myVariable * 4 << endl;
+
+    return 0;
+}
